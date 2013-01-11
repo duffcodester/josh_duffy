02ce48b78db849f4bbfa96d6c6f019fcd2c2d97e
Josh Duffy <jduffy@mines.edu>
2013-01-10 18:11:33 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JoshDuffy::Application.config.secret_token = '10f754637853d6374a496316f5851c750a06de0f3a3c6ed27445b52f4b18294ced9cfeffe3dacf2a924bfc26d73a85e2508c7a0e6148421fbc8f3fe2e6cf4dc5'
